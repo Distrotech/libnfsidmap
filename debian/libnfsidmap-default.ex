272df2312f5cc97c28c3cac92e12df3bd8483246
bfields <bfields>
2004-08-09 16:34:38 +0000

    make it possible to build a .deb from cvs

---
+++ debian/libnfsidmap-default.ex
@@ -0,0 +1,10 @@
+# Defaults for libnfsidmap initscript
+# sourced by /etc/init.d/libnfsidmap
+# installed at /etc/default/libnfsidmap by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ debian/shlibs.local.ex
@@ -0,0 +1,1 @@
+liblibnfsidmap 0.1 libnfsidmap (>> 0.1-0), libnfsidmap (<< 0.1-99)
